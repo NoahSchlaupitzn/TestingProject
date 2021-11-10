3123fec7350158a976bb152fe2ed558b38b86152
NoahSchlaupitzn <85567568+NoahSchlaupitzn@users.noreply.github.com>
2021-11-10 13:06:56 -0500

    testing second commit

---
+++ BackEndTestingProject/src/main/java/com/Testing/BackEndTestingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndTestingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndTestingProjectApplication.class, args);
+	}
+
+}
